2fc22c2a7fa42d6c38eb8ead677181b04f2a8fcd
paulokatho <pkatho@gmail.com>
2017-03-01 12:24:16 -0300

    terminando cadastro de vinhos com mensagem de sucesso e começando a pagina de pesquisa de vinhos

--- src/main/java/com/katho/vinhos/controller/VinhosController.java
+++ src/main/java/com/katho/vinhos/controller/VinhosController.java
@@ -7,33 +7,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.katho.vinhos.model.TipoVinho;
 import com.katho.vinhos.model.Vinho;
 import com.katho.vinhos.repository.Vinhos;
 
 @Controller
+@RequestMapping("/vinhos")
 public class VinhosController {
 
 	@Autowired
 	private Vinhos vinhos;
 	
-	@GetMapping("/vinhos/novo")
+	@GetMapping("/novo")
 	public ModelAndView novo(Vinho vinho) {
 		ModelAndView mv = new ModelAndView("vinho/cadastro-vinho");
 		mv.addObject("tipos", TipoVinho.values());
 		return mv;
 	}
 	
-	@PostMapping("/vinhos/novo")
-	public ModelAndView salvar(@Valid Vinho vinho, BindingResult result) {
+	@PostMapping("/novo")
+	public ModelAndView salvar(@Valid Vinho vinho, BindingResult result,
+								RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			 return novo(vinho);
-		}
-		
+		}		
 		vinhos.save(vinho);
+		attributes.addFlashAttribute("mensagem", "Vinho salvo com sucesso!");
+		
 		return new ModelAndView("redirect:/vinhos/novo");
+	}
+	
+	@GetMapping // quando digitar essa url ele cai nesse método
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("/vinhos/pesquisa-vinhos");
+		mv.addObject("vinhos", vinhos.findAll());
+		return mv;
 	}
 	
 	
